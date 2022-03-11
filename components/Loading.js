d7fda76e5af91c0379698b29996a1d365a77ed50
Ushan Fernando <ushansankalpafernando@gmail.com>
2022-03-11 19:53:25 +0530

    fixed bugs

--- components/Loading.js
+++ components/Loading.js
@@ -13,11 +13,11 @@ function Loading() {
         www.Ushan.me
       </h2>
       <h2 className="text-white text-3xl text-center">Please Wait....</h2>
-      <div class="flex items-center justify-center mt-8">
-        <div class="flex items-center justify-center space-x-2 animate-pulse">
-          <div class="bg-white p-2  w-8 h-8 rounded-full animate-bounce blue-circle"></div>
-          <div class="bg-white p-2 w-8 h-8 rounded-full animate-bounce green-circle"></div>
-          <div class="bg-white p-2  w-8 h-8 rounded-full animate-bounce red-circle"></div>
+      <div className="flex items-center justify-center mt-8">
+        <div className="flex items-center justify-center space-x-2 animate-pulse">
+          <div className="bg-white p-2  w-8 h-8 rounded-full animate-bounce blue-circle"></div>
+          <div className="bg-white p-2 w-8 h-8 rounded-full animate-bounce green-circle"></div>
+          <div className="bg-white p-2  w-8 h-8 rounded-full animate-bounce red-circle"></div>
         </div>
       </div>
     </div>
